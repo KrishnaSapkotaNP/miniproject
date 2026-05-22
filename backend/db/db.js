1165989499d4377d00b2b6e6521603dbffdc7f8f
KrishnaSapkotaNP <sapkotak986@gmail.com>
2026-05-23 02:03:43 +0530

    Padne kati Padne

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,19 +1,15 @@
-import pkg from 'pg';
-const { Pool } = pkg;
+import pg from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-});
+console.log(process.env.DATABASE_URL);
 
-pool.on('error', (err) => {
-  console.error('Unexpected error on idle client', err);
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default pool;
